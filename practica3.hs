2dd0cb4c6cbdad94644d9f7e00caf75f3f23b698
alejandro_jacome <jacomedelgadoa@ciencias.unam.mx>
2022-10-01 16:43:04 -0500

    formato de para los ejercicios

--- practica3.hs
+++ practica3.hs
@@ -9,12 +9,9 @@ las listas que tengan "a" van a ser listas con pares -}
 
 longP :: [a] -> Int 
 
-longP [a] = 2 {- aqui decimos que cuando la lista solo tiene un par, 
-me regrese que tiene un valor de 2, pues en ese par hay 2 elementos -}
+longP [a] = 2 
 
-longP (x:xs) = 2 + longP xs {- aqui le decimos que cuando le entregamos 
-una lista con cabeza y cola, cuente 2 y le sume el valor de la funcion 
-longP de la cola (paso recursivo-}
+longP (x:xs) = 2 + longP xs 
 
 --ejercicio 2
 
